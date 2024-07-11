5ee87f0fb89095e2aa2202930f103176aadc5f49
Daniel J. Szelogowski <dszelogowski@gmail.com>
2024-07-10 21:43:16 -0500

    Initial commit

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from setuptools import setup, find_packages
 
+# Be sure to update the version number in both "setup.py" and "meta.yaml" files.
 setup(
     name='pyorganoid',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     author='Daniel Szelogowski',
     description='A Python package for the simulation of organoids for the purpose of studying '
                 'Organoid Intelligence (OI) and Organoid Learning (OL).',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
+    url='https://danielathome19.github.io/pyorganoid',
+    project_urls={
+        'Documentation': 'https://danielathome19.github.io/pyorganoid',
+        'Source': 'https://github.com/danielathome19/pyorganoid',
+        'Tracker': 'https://github.com/danielathome19/pyorganoid/issues',
+    },
     install_requires=[
         'numpy',
         'matplotlib',
